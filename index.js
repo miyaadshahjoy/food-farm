c572023fecc9d8a2690a98911f29c798970ce7a9
miyaadjoy <miyaadjoy177@gmail.com>
2024-09-03 05:52:55 +0600

    PORT CHANGED

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  'utf-8',
+  'utf-8'
 );
 const templateProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  'utf-8',
+  'utf-8'
 );
 const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  'utf-8',
+  'utf-8'
 );
 
 const data = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 );
-const replaceTemplate = function (template, data) {
+
+const replaceTemplate = (template, data) => {
   let output = template.replace(/{%PRODUCTNAME%}/g, data.productName);
   output = output.replace(/{%ID%}/g, data.id);
   output = output.replace(/{%IMAGE%}/g, data.image);
@@ -34,20 +35,14 @@ const replaceTemplate = function (template, data) {
 
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url, true);
-
   const id = +url.parse(req.url, true).query.id;
 
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, {
-      'content-type': 'text/html',
-    });
-    const cards = data.map((ob) => {
-      return replaceTemplate(templateCard, ob);
-    });
-    const output = templateOverview.replace(/{%CARDS%}/g, cards.join());
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const cards = data.map((ob) => replaceTemplate(templateCard, ob));
+    const output = templateOverview.replace(/{%CARDS%}/g, cards.join(''));
     res.end(output);
-  }
-  if (pathname === '/product') {
+  } else if (pathname === '/product') {
     const product = data.find((ob) => ob.id === id);
     if (!product) {
       res.statusCode = 404;
@@ -55,13 +50,14 @@ const server = http.createServer((req, res) => {
       return;
     }
     const output = replaceTemplate(templateProduct, product);
-    res.writeHead(200, {
-      'content-type': 'text/html',
-    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(output);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('Page not found');
   }
 });
 
-server.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server is running at : http://127.0.0.1:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running at: http://127.0.0.1:${PORT}`);
 });
